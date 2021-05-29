1b2b6ca29a19e9e03d43270ede3e0d86ce3566fc
Norman-w <85535885@qq.com>
2021-05-29 12:13:17 +0800

    从github网页获取到了最新的文件,然后再修改一下,push回去,测试完以后就算完成了.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className={'main'}>
       <div>This is my first git test program witch create by webstorm</div>
         <div>And now, I'm trying to add some text here in order to test the update</div>
+        <div>现在,我已经掌握了webstorm+github的基本使用.2021年05月29日12:12:57</div>
     </div>
   );
 }
