b519d0acc8a50dc50b6b06fc664d5f701b68d9a3
Norman-w <85535885@qq.com>
2021-05-29 10:53:50 +0800

    add some text to here

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className={'main'}>
-      This is my first git test program witch create by webstorm
+      <div>This is my first git test program witch create by webstorm</div>
+        <div>And now, I'm trying to add some text here in order to test the update</div>
     </div>
   );
 }
